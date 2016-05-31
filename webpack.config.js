fa66aff2268854be1c7dcdfb1e5d918d180144d1
Mike Frazier <michael.s.frazier@gmail.com>
2016-05-31 14:48:56 -0500

    Add CSS handling dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -22,4 +22,10 @@ var config = getConfig({
   clearBeforeBuild: true
 });
 
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+  ]);
+
 module.exports = config;

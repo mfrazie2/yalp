49f4cddc45e0dabe4b2fb41f8c15d4fcd498dc55
Mike Frazier <michael.s.frazier@gmail.com>
2016-06-01 15:49:43 -0500

    Set up basic routing and spec test

--- src/app.js
+++ src/app.js
@@ -4,6 +4,21 @@ import 'font-awesome/css/font-awesome.css'
 import './app.css'
 import App from 'containers/App/App'
 
+import { browserHistory, Router, Route } from 'react-router'
+
+const Home = React.createClass({
+  render() {
+    return (
+      <div>Hello World</div>  
+    );
+  }
+});
+
+const routes = (
+  <Router>
+    <Route path='/' component={Home} />
+  </Router>
+);
 
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(<App history={browserHistory} routes={routes} />, mountNode);
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { Router } from 'react-router'
 import styles from './styles.module.css'
 
-const App = React.createClass({
+class App extends React.Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    routes: PropTypes.object.isRequired
+  }
+  
+  get content () {
+    return (
+      <Router 
+        history={this.props.history} 
+        routes={this.props.routes}  
+      />
+    );
+  }
+  
   render () {
     return (
-      <div className={styles.wrapper} >
-        Text Text Text
-        <i className="fa fa-star"></i>
-        <h1> Environment: {__NODE_ENV__} </h1>
+      <div style= {{ height: '100%' }}>
+        {this.content}
       </div>
     );
   }
-});
+};
 
 export default App;

4107e54d9210c33c52fb6fbe708f54cfdb604cbe
Mike Frazier <michael.s.frazier@gmail.com>
2016-06-01 15:55:32 -0500

    Move routing to separate file

--- src/app.js
+++ src/app.js
@@ -4,21 +4,10 @@ import 'font-awesome/css/font-awesome.css'
 import './app.css'
 import App from 'containers/App/App'
 
-import { browserHistory, Router, Route } from 'react-router'
+import { browserHistory } from 'react-router'
+import makeRoutes from './routes'
 
-const Home = React.createClass({
-  render() {
-    return (
-      <div>Hello World</div>  
-    );
-  }
-});
-
-const routes = (
-  <Router>
-    <Route path='/' component={Home} />
-  </Router>
-);
+const routes = makeRoutes();
 
 const mountNode = document.querySelector('#root');
 ReactDOM.render(<App history={browserHistory} routes={routes} />, mountNode);
